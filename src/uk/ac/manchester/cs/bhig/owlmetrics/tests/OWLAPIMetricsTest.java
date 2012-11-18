6d64156259013539d559fade74b34c89dba6802b
Eleni Mikroyannidi <mikrohelen@gmail.com>
2012-11-18 12:33:30 +0000

    Updates on the libraries.

--- src/uk/ac/manchester/cs/bhig/owlmetrics/tests/OWLAPIMetricsTest.java
+++ src/uk/ac/manchester/cs/bhig/owlmetrics/tests/OWLAPIMetricsTest.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Set;
 
+import org.semanticweb.owlapi.metrics.AxiomTypeCountMetricFactory;
 import org.semanticweb.owlapi.metrics.DLExpressivity;
 import org.semanticweb.owlapi.metrics.GCICount;
 import org.semanticweb.owlapi.metrics.HiddenGCICount;
@@ -12,23 +13,19 @@ import org.semanticweb.owlapi.metrics.LogicalAxiomCount;
 import org.semanticweb.owlapi.metrics.MaximumNumberOfNamedSuperclasses;
 import org.semanticweb.owlapi.metrics.NumberOfClassesWithMultipleInheritance;
 import org.semanticweb.owlapi.metrics.OWLMetric;
-import org.semanticweb.owlapi.metrics.AxiomTypeCountMetricFactory;
+
 import uk.ac.manchester.cs.bhig.owlmetrics.utilities.OWLExtendedMetricManager;
 
-
-public class OWLAPIMetricsTest{
-	
-	
+public class OWLAPIMetricsTest {
 
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
 		OWLOntologyLoader l = new OWLOntologyLoader(args[0]);
-		
+
 		List<OWLMetric<?>> metrics = new ArrayList<OWLMetric<?>>();
-		
-		
+
 		metrics.add(new DLExpressivity(l.getManager()));
 		metrics.add(new GCICount(l.getManager()));
 		metrics.add(new ImportClosureSize(l.getManager()));
@@ -36,15 +33,16 @@ public class OWLAPIMetricsTest{
 		metrics.add(new LogicalAxiomCount(l.getManager()));
 		metrics.add(new MaximumNumberOfNamedSuperclasses(l.getManager()));
 		metrics.add(new NumberOfClassesWithMultipleInheritance(l.getManager()));
-		
-		Set<OWLMetric<?>> axiomTypeMetrics = AxiomTypeCountMetricFactory.createMetrics(l.getManager());
+
+		Set<OWLMetric<?>> axiomTypeMetrics = AxiomTypeCountMetricFactory
+				.createMetrics(l.getManager());
 		metrics.addAll(axiomTypeMetrics);
-		
+
 		OWLExtendedMetricManager manager = new OWLExtendedMetricManager(metrics);
 		manager.setOntology(l.getOntology());
-		
+
 		System.out.println(manager.toString());
-		
+
 	}
 
 }
--- src/uk/ac/manchester/cs/bhig/owlmetrics/utilities/OWLExtendedMetricManager.java
+++ src/uk/ac/manchester/cs/bhig/owlmetrics/utilities/OWLExtendedMetricManager.java
@@ -6,24 +6,25 @@ import org.semanticweb.owlapi.metrics.OWLMetric;
 import org.semanticweb.owlapi.metrics.OWLMetricManager;
 
 public class OWLExtendedMetricManager extends OWLMetricManager {
-	
-	private List<OWLMetric<?>> metrics;
+
+	private final List<OWLMetric<?>> metrics;
 
 	public OWLExtendedMetricManager(List<OWLMetric<?>> metrics) {
 		super(metrics);
 		this.metrics = metrics;
 	}
-	
+
+	@Override
 	public String toString() {
-        StringBuilder sb = new StringBuilder();
-        int counter=1;
-        for (OWLMetric m : metrics) {
-        	sb.append(counter + ") ");
-            sb.append(m);
-            sb.append("\n");
-            counter++;
-        }
-        return sb.toString();
-    }
+		StringBuilder sb = new StringBuilder();
+		int counter = 1;
+		for (OWLMetric m : metrics) {
+			sb.append(counter + ") ");
+			sb.append(m);
+			sb.append("\n");
+			counter++;
+		}
+		return sb.toString();
+	}
 
 }
